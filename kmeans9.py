59361c7145ed081e7596047d6ccdb97323e6effe
horng <horng@hust.edu.tw>
2023-08-01 21:54:28 +0800

    Add files via upload

---
+++ kmeans9.py
@@ -0,0 +1,18 @@
+#https://www.tutorialspoint.com/machine_learning_with_python/clustering_algorithms_k_means_algorithm.htm
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+digits = load_digits()
+digits.data.shape
+print(digits.data.shape)
+kmeans = KMeans(n_clusters=10, random_state=0)
+clusters = kmeans.fit_predict(digits.data)
+kmeans.cluster_centers_.shape
+fig, ax = plt.subplots(2, 5, figsize=(8, 3))
+centers = kmeans.cluster_centers_.reshape(10, 8, 8)
+for axi, center in zip(ax.flat, centers):
+   axi.set(xticks=[], yticks=[])
+   axi.imshow(center, interpolation='nearest', cmap=plt.cm.binary)
+plt.show()
